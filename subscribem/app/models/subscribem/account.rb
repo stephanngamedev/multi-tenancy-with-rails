6f18f8ada02a6a90594f781590e766dbc879c1e8
Stephann Vasconcelos <stephannvasconcelos@gmail.com>
2014-10-29 10:08:42 -0300

    2.6 - restrictiong sign in by account

--- subscribem/app/models/subscribem/account.rb
+++ subscribem/app/models/subscribem/account.rb
@@ -1,15 +1,28 @@
 module Subscribem
 	class Account < ActiveRecord::Base
-		EXCLUDED_SUBDOMAINS = %w(admin)
 		belongs_to :owner, class_name: 'Subscribem::User'
 		accepts_nested_attributes_for :owner
+
+		has_many :members, class_name: 'Subscribem::Member'
+		has_many :users, through: :members
 
 		before_validation do
 			self.subdomain = subdomain.to_s.downcase
 		end
 
 		validates :subdomain, presence: true, uniqueness: true
+		
+		EXCLUDED_SUBDOMAINS = %w(admin)
 		validates_exclusion_of :subdomain, in: EXCLUDED_SUBDOMAINS, message: 'is not allowed. Please choose another subdomain.'
+		
 		validates_format_of :subdomain, with: /\A[\w\-]+\Z/i, message: 'is not allowed. Please choose another subdomain.'
+
+		def self.create_with_owner( params = {} )
+			account = new(params)
+			if account.save
+				account.users << account.owner
+			end
+			account
+		end
 	end
 end
